7fd482a98090da503a2af860d2e524db8af73ba5
HASNAIN KHAN <ak8876575@gmail.com>
2025-03-07 16:50:34 +0530

    Updated Work

--- src/constants/index.js
+++ src/constants/index.js
@@ -52,7 +52,7 @@ const services = [
     icon: backend,
   },
   {
-    title: "Content Creator",
+    title: "UI/UX Designer",
     icon: creator,
   },
 ];
@@ -118,7 +118,7 @@ const experiences = [
     company_name: "SVIET",
     icon: sviet,
     iconBg: "#383E56",
-    date: "March 2020 - April 2021",
+    date: "March 2023 - Present",
     points: [
       "Developing and maintaining web applications using React.js and other related technologies.",
       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
@@ -131,7 +131,7 @@ const experiences = [
     company_name: "Super 60",
     icon: s60,
     iconBg: "#E6DEDD",
-    date: "Jan 2021 - Feb 2022",
+    date: "Dec 2023 - Present",
     points: [
       "Developing and maintaining web applications using React.js and other related technologies.",
       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
@@ -144,7 +144,7 @@ const experiences = [
     company_name: "Super 60",
     icon: s60,
     iconBg: "#383E56",
-    date: "Jan 2022 - Jan 2023",
+    date: "Dec 2023 - Present",
     points: [
       "Developing and maintaining web applications using React.js and other related technologies.",
       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
@@ -157,7 +157,7 @@ const experiences = [
     company_name: "SVIET",
     icon: sviet,
     iconBg: "#E6DEDD",
-    date: "Jan 2023 - Present",
+    date: "March 2023 - Present",
     points: [
       "Developing and maintaining web applications using React.js and other related technologies.",
       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
